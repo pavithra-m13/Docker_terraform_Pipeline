a8f601bc1c81b1837d6edbe89673b983cead1e5a
pavithra <pavithra.25cs@licet.ac.in>
2025-02-21 12:29:03 +0000

    Commit

---
+++ terraform/jenkins_deploy.tf
@@ -0,0 +1,22 @@
+# Pull Jenkins Docker Image
+resource "docker_image" "jenkins" {
+  name = "jenkins/jenkins:lts-jdk17"
+}
+
+resource "docker_container" "jenkins_server" {
+  image = docker_image.jenkins.image_id
+  name  = "jenkins-container"
+
+  ports {
+    internal = 8080
+    external = 9091
+  }
+
+  volumes {
+    host_path      = "/var/jenkins_home"
+    container_path = "/var/jenkins_home"
+  }
+
+  privileged = true   
+  restart    = "always"  
+}
