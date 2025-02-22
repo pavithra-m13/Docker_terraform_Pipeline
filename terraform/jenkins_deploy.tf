15eba92b5de9fb52f3d63fff2ac328ee5232bfe6
pavithra-m13 <138682484+pavithra-m13@users.noreply.github.com>
2025-02-22 21:57:38 +0530

    Update jenkins_deploy.tf

--- terraform/jenkins_deploy.tf
+++ terraform/jenkins_deploy.tf
@@ -1,8 +1,6 @@
-  GNU nano 7.2                                          jenkins_deploy.tf                                                   # Pull Jenkins Docker Image
 resource "docker_image" "jenkins" {
   name = "jenkins/jenkins:lts-jdk17"
 }
-
 resource "docker_container" "jenkins_server" {
   image = docker_image.jenkins.image_id
   name  = "jenkins-container"
@@ -17,6 +15,16 @@ resource "docker_container" "jenkins_server" {
     container_path = "/var/jenkins_home"
   }
 
-  privileged = true
-  restart    = "always"
+  # Mount Docker socket to allow Jenkins to use host's Docker
+  volumes {
+    host_path      = "/var/run/docker.sock"
+    container_path = "/var/run/docker.sock"
+  }
+  # Mount the Docker CLI binary (so Jenkins can use the `docker` command)
+  volumes {
+    host_path      = "/usr/bin/docker"
+    container_path = "/usr/bin/docker"
+  }
+  privileged   = true
+  restart      = "always"
 }
