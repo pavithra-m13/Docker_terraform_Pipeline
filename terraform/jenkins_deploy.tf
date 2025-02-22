3d9c10f360c371c2621ab48657bfb89e62034574
pavithra-m13 <138682484+pavithra-m13@users.noreply.github.com>
2025-02-22 18:06:53 +0530

    Update jenkins_deploy.tf

--- terraform/jenkins_deploy.tf
+++ terraform/jenkins_deploy.tf
@@ -3,6 +3,7 @@ resource "docker_image" "jenkins" {
   name = "jenkins/jenkins:lts-jdk17"
 }
 
+# Run Jenkins Container
 resource "docker_container" "jenkins_server" {
   image = docker_image.jenkins.image_id
   name  = "jenkins-container"
@@ -17,6 +18,20 @@ resource "docker_container" "jenkins_server" {
     container_path = "/var/jenkins_home"
   }
 
+  # Mount Docker socket to allow Jenkins to access the Docker engine
+  volumes {
+    host_path      = "/var/run/docker.sock"
+    container_path = "/var/run/docker.sock"
+  }
+
   privileged = true   
   restart    = "always"  
+
+  # Install Docker inside the Jenkins container automatically
+  provisioner "remote-exec" {
+    inline = [
+      "apt-get update && apt-get install -y docker.io",
+      "usermod -aG docker jenkins"
+    ]
+  }
 }
